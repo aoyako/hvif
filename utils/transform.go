936835243aa54cc4b5550e6b0988255387d4a1d1
aoyako <ml.logachevsky@gmail.com>
2024-03-30 21:33:02 +0900

    format transform and utils packages

--- utils/transform.go
+++ utils/transform.go
@@ -3,49 +3,28 @@ package utils
 import (
 	"encoding/binary"
 	"io"
-	"math"
 )
-
-func ReadFloatCoord(r io.Reader) float32 {
-	var val uint16
-	var x uint8
-	binary.Read(r, binary.LittleEndian, &x)
-	val = uint16(x)
-	if val&0x80 != 0 {
-		var xlow uint8
-		binary.Read(r, binary.LittleEndian, &xlow)
-		val = val & (0x80 - 1)
-		val = (val << 8) | uint16(xlow)
-
-		return float32(val)/102.0 - 128.0
-	}
-
-	return float32(val) - 32.0
-}
-
-func ReadFloatTrans(r io.Reader) float32 {
-	var b1 uint8
-	var b2 uint8
-	var b3 uint8
-	binary.Read(r, binary.LittleEndian, &b1)
-	binary.Read(r, binary.LittleEndian, &b2)
-	binary.Read(r, binary.LittleEndian, &b3)
-
-	value := uint32(uint32(b1)<<16 | uint32(b2)<<8 | uint32(b3))
-	if value == 0 {
-		return 0.0
-	}
-
-	sign := ((value & 0b100000000000000000000000) >> 23)
-	expo := ((value & 0b011111100000000000000000) >> 17) - 32
-	mant := ((value & 0b000000011111111111111111) >> 6)
-
-	bits := (sign << 31) | ((expo + 127) << 23) | mant
-	return math.Float32frombits(bits)
-}
 
 const TransformMatrixSize = 6
 const PerspectiveMatrixSize = 9
+
+type LineJoinOptions uint8
+
+const (
+	MiterJoin LineJoinOptions = iota
+	MiterJoinRevert
+	RoundJoin
+	BevelJoin
+	MiterJoinRound
+)
+
+type LineCapOptions uint8
+
+const (
+	ButtCap LineCapOptions = iota
+	SquareCap
+	RoundCap
+)
 
 type TransformerType uint8
 
@@ -70,12 +49,27 @@ type LodScale struct {
 	MaxS float32
 }
 
-func ReadMatrix(r io.Reader, size int) []float32 {
-	res := make([]float32, size)
-	for i := 0; i < size; i++ {
-		res[i] = ReadFloatTrans(r)
-	}
-	return res
+type Transformer any
+
+type TransformerAffine struct {
+	Matrix [TransformMatrixSize]float32
+}
+
+type TransformerPerspective struct {
+	Matrix [PerspectiveMatrixSize]float32
+}
+
+type TransformerContour struct {
+	Width      float32
+	LineJoin   LineJoinOptions
+	MiterLimit float32
+}
+
+type TransformerStroke struct {
+	Width      float32
+	LineJoin   LineJoinOptions
+	LineCap    LineCapOptions
+	MiterLimit float32
 }
 
 func ReadTransformable(r io.Reader) Transformable {
@@ -84,30 +78,10 @@ func ReadTransformable(r io.Reader) Transformable {
 	return t
 }
 
-type TransformerAffine struct {
-	Matrix [TransformMatrixSize]float32
-}
-
 func ReadAffine(r io.Reader) TransformerAffine {
 	var t TransformerAffine
 	copy(t.Matrix[:], ReadMatrix(r, TransformMatrixSize))
 	return t
-}
-
-type LineJoinOptions uint8
-
-const (
-	MiterJoin LineJoinOptions = iota
-	MiterJoinRevert
-	RoundJoin
-	BevelJoin
-	MiterJoinRound
-)
-
-type TransformerContour struct {
-	Width      float64
-	LineJoin   LineJoinOptions
-	MiterLimit float64
 }
 
 func ReadCountour(r io.Reader) TransformerContour {
@@ -119,35 +93,16 @@ func ReadCountour(r io.Reader) TransformerContour {
 	binary.Read(r, binary.LittleEndian, &lineJoin)
 	binary.Read(r, binary.LittleEndian, &miterLimit)
 
-	t.Width = (float64(width) - 128.0)
+	t.Width = (float32(width) - 128.0)
 	t.LineJoin = LineJoinOptions(lineJoin)
-	t.MiterLimit = float64(miterLimit)
+	t.MiterLimit = float32(miterLimit)
 	return t
-}
-
-type TransformerPerspective struct {
-	Matrix [PerspectiveMatrixSize]float32
 }
 
 func ReadTransformerPerspective(r io.Reader) TransformerPerspective {
 	var t TransformerPerspective
 	copy(t.Matrix[:], ReadMatrix(r, TransformMatrixSize))
 	return t
-}
-
-type LineCapOptions uint8
-
-const (
-	ButtCap LineCapOptions = iota
-	SquareCap
-	RoundCap
-)
-
-type TransformerStroke struct {
-	Width      float32
-	LineJoin   LineJoinOptions
-	LineCap    LineCapOptions
-	MiterLimit float32
 }
 
 func ReadTransformerStroke(r io.Reader) TransformerStroke {
@@ -166,8 +121,6 @@ func ReadTransformerStroke(r io.Reader) TransformerStroke {
 
 	return t
 }
-
-type Transformer any
 
 func ReadTransformer(r io.Reader) Transformer {
 	var ttype TransformerType
---
+++ utils/utils.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"encoding/binary"
+	"io"
+	"math"
+)
+
+func ReadFloatCoord(r io.Reader) float32 {
+	var val uint16
+	var x uint8
+	binary.Read(r, binary.LittleEndian, &x)
+	val = uint16(x)
+	if val&0x80 != 0 {
+		var xlow uint8
+		binary.Read(r, binary.LittleEndian, &xlow)
+		val = val & (0x80 - 1)
+		val = (val << 8) | uint16(xlow)
+
+		return float32(val)/102.0 - 128.0
+	}
+
+	return float32(val) - 32.0
+}
+
+func ReadFloat24(r io.Reader) float32 {
+	var b1 uint8
+	var b2 uint8
+	var b3 uint8
+	binary.Read(r, binary.LittleEndian, &b1)
+	binary.Read(r, binary.LittleEndian, &b2)
+	binary.Read(r, binary.LittleEndian, &b3)
+
+	value := uint32(uint32(b1)<<16 | uint32(b2)<<8 | uint32(b3))
+	if value == 0 {
+		return 0.0
+	}
+
+	sign := ((value & 0b100000000000000000000000) >> 23)
+	expo := ((value & 0b011111100000000000000000) >> 17) - 32
+	mant := ((value & 0b000000011111111111111111) >> 6)
+
+	bits := (sign << 31) | ((expo + 127) << 23) | mant
+	return math.Float32frombits(bits)
+}
+
+func ReadMatrix(r io.Reader, size int) []float32 {
+	res := make([]float32, size)
+	for i := 0; i < size; i++ {
+		res[i] = ReadFloat24(r)
+	}
+	return res
+}
